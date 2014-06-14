0bc15e39f402a50c29ddc31196c47da0015607ae
Suman Raj Venkatesan <sumanraj.v@gmail.com>
2014-06-14 12:54:24 +0530

    http client library for iOS and OSX

---
+++ SRWebClient/SRWebClient.swift
@@ -0,0 +1,188 @@
+//
+//  SRWebClient.swift
+//  SampleApp
+//
+//  Created by Suman Raj on 07/06/14.
+//  Copyright (c) 2014 Suman Raj. All rights reserved.
+//
+
+import Foundation
+
+class SRWebClient : NSObject
+{
+    typealias Headers = Dictionary<String, String>
+    typealias RequestData = Dictionary<String,AnyObject>
+    typealias SuccessHandler = (AnyObject!, Int) -> Void
+    typealias FailureHandler = (NSError!) -> Void
+    
+    var operationQueue: NSOperationQueue
+    var urlRequest: NSMutableURLRequest? = nil
+    
+    let timeoutInterval:NSTimeInterval = 30.0
+    
+    /**
+    *  GET class methods
+    */
+    class func GET(url: String) -> SRWebClient {
+        return SRWebClient(method: "GET", url: url)
+    }
+    
+    class func GET(url: String, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.GET(url).send(success, failure)
+    }
+    
+    class func GET(url: String, data:RequestData?, headers:Headers?) -> SRWebClient {
+        return SRWebClient.GET(url).headers(headers).data(data, url: url)
+    }
+    
+    class func GET(url: String, data:RequestData?, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.GET(url, data: data, headers:nil).send(success, failure)
+    }
+    
+    class func GET(url: String, data:RequestData?, headers:Headers?, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.GET(url, data: data, headers:headers).send(success, failure)
+    }
+    
+    /**
+    *  POST class methods
+    */
+    class func POST(url: String) -> SRWebClient {
+        return SRWebClient(method: "POST", url: url)
+    }
+    
+    class func POST(url: String, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.POST(url).send(success, failure)
+    }
+    
+    class func POST(url: String, data:RequestData?, headers:Headers?) -> SRWebClient {
+        return SRWebClient.POST(url).headers(headers).data(data, url:url)
+    }
+    
+    class func POST(url: String, data:RequestData?, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.POST(url, data: data, headers:nil).send(success, failure)
+    }
+    
+    class func POST(url: String, data:RequestData?, headers:Headers?, success:SuccessHandler?, failure:FailureHandler?) -> SRWebClient {
+        return SRWebClient.POST(url, data: data, headers:headers).send(success, failure)
+    }
+    
+    /**
+    *  Instance initialization
+    */
+    init(method:String, url:String) {
+        self.operationQueue = NSOperationQueue()
+        self.urlRequest = NSMutableURLRequest(URL: NSURL(string: url))
+        self.urlRequest!.HTTPMethod = method
+        self.urlRequest!.timeoutInterval = timeoutInterval
+    }
+    
+    /**
+    *  Function to set headers
+    *
+    *  @param Headers? optional value of type Dictionary<String, String>
+    *
+    *  @return self instance to support function chaining
+    */
+    func headers(headers: Headers?) -> SRWebClient {
+        if (headers) {
+            self.urlRequest!.allHTTPHeaderFields = headers!
+        }
+        return self
+    }
+    
+    /**
+    *  Function to set data for GET/POST request
+    *
+    *  @param data:RequestData? optional value of type Dictionary<String,AnyObject>
+    *  @param url:String        request url for GET request
+    *
+    *  @return self instance to support function chaining
+    */
+    func data(data:RequestData?, url:String) -> SRWebClient {
+        if (data && data!.count > 0) {
+            if(self.urlRequest!.HTTPMethod == "GET") {
+                self.urlRequest!.URL = NSURL(string: url + "?" + self.build(data)!)
+            } else if(self.urlRequest!.HTTPMethod == "POST") {
+                self.urlRequest!.HTTPBody  = self.buildPost(data)!
+            }
+        }
+        return self
+    }
+    
+    /**
+    *  Function to cancel request operation
+    */
+    func cancel() {
+        if(self.operationQueue.operationCount > 0) {
+            self.operationQueue.cancelAllOperations()
+        }
+    }
+    
+    /**
+    *  Function to build get request E.g., a dictionary of ["a":"b","c":"d"] will return "a=b&c=d"
+    *
+    *  @param dataDict:RequestData? request data
+    *
+    *  @return of type String
+    */
+    func build(dataDict:RequestData?) -> String? {
+        var dataList: String[] = String[]()
+        if(dataDict) {
+            for (key, value : AnyObject) in dataDict! {
+                dataList.append("\(key)=\(value)")
+            }
+        }
+        return join("&", dataList)
+    }
+    
+    /**
+    *  Function to build post request and returns NSData object
+    *
+    *  @param dataDict:RequestData? request data
+    *
+    *  @return of type NSData
+    */
+    func buildPost(dataDict:RequestData?) -> NSData? {
+        return self.build(dataDict)!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+    }
+    
+    /**
+    *  Function to send request to endpoint url, creates NSBlockOperation and add it to NSOperationQueue to execture the task
+    *  and calls the respective handlers when ever we get data or failure.
+    *
+    *  @param successHandler:SuccessHandler? block of type (AnyObject!, Int) -> Void
+    *  @param failureHandler:FailureHandler? block of type (NSError!) -> Void
+    *
+    *  @return self instance to support function chaining
+    */
+    func send(successHandler:SuccessHandler?, failureHandler:FailureHandler?) -> SRWebClient {
+        var blockOperation:NSBlockOperation = NSBlockOperation({() -> Void in
+            
+            var response:NSURLResponse?
+            var error:NSError?
+            
+            let result:NSData? = NSURLConnection.sendSynchronousRequest(self.urlRequest, returningResponse: &response, error: &error)
+            let httpResponse:NSHTTPURLResponse? = response as? NSHTTPURLResponse
+            
+            if(response && httpResponse!.statusCode >= 200 && httpResponse!.statusCode <= 300) {
+                if(NSJSONSerialization.isValidJSONObject(result)) {
+                    let json : AnyObject! = NSJSONSerialization.JSONObjectWithData(result, options: nil, error: &error)
+                    if (error && failureHandler) {
+                        failureHandler!(error)
+                    } else if (json) {
+                        successHandler!(json, httpResponse!.statusCode)
+                    }
+                } else if (successHandler) {
+                    successHandler!(NSString(data: result, encoding: NSUTF8StringEncoding), httpResponse!.statusCode)
+                }
+            } else if (response && httpResponse && failureHandler) {
+                failureHandler!(NSError(domain: self.urlRequest!.URL.path, code: httpResponse!.statusCode, userInfo: nil))
+            } else if (failureHandler) {
+                failureHandler!(error)
+            }
+        })
+        
+        self.operationQueue.addOperation(blockOperation)
+        return self
+    }
+}
---
+++ SRWebClientTests/SRWebClientTests.swift
@@ -0,0 +1,179 @@
+//
+//  SRWebClientTests.swift
+//  SRWebClientTests
+//
+//  Created by Suman Raj on 13/06/14.
+//  Copyright (c) 2014 Suman Raj. All rights reserved.
+//
+
+import XCTest
+import SRWebClient
+
+class SRWebClientTests: XCTestCase {
+    
+    var resDict:Dictionary<String, String>? = nil
+    
+    override func setUp() {
+        self.resDict = [
+            "Accept-Language": "en-us",
+            "Host": "headers.jsontest.com",
+            "User-Agent": "xctest (unknown version) CFNetwork/695.1.5 Darwin/13.2.0",
+            "Accept": "*/*"
+        ]
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        resDict = nil
+        super.tearDown()
+    }
+    
+    func waitFor (inout wait: Bool) {
+        while (wait) {
+            NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 0.1))
+        }
+    }
+    
+    func testGetSuccess() {
+        var wait: Bool = true
+        SRWebClient.GET("http://headers.jsontest.com/",
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNotNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNil(error)
+                wait = false
+        })
+        self.waitFor(&wait)
+    }
+    
+    func testGetDataSuccess() {
+        var wait: Bool = true
+        SRWebClient.GET("http://ip.jsontest.com/", data: ["q":"search"],
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNotNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNil(error)
+                wait = false
+            })
+        self.waitFor(&wait)
+    }
+
+    func testGetFailure() {
+        var wait: Bool = true
+        SRWebClient.GET("http://failure.example.com/",
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNotNil(error)
+                wait = false
+        })
+        self.waitFor(&wait)
+    }
+    
+    func testGetDataFailure() {
+        var wait: Bool = true
+        SRWebClient.GET("http://failure.example.com/", data: ["q":"search"],
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNotNil(error)
+                wait = false
+            })
+        self.waitFor(&wait)
+    }
+    
+    func testPostSuccess() {
+        var wait: Bool = true
+        let jsonData = NSJSONSerialization.dataWithJSONObject(["1","2","3"], options: nil, error: nil)
+        let jsonString = NSString(data: jsonData, encoding: NSUTF8StringEncoding)
+        SRWebClient.POST("http://validate.jsontest.com/", data: ["json":jsonString],
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNotNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNil(error)
+                wait = false
+            })
+        self.waitFor(&wait)
+    }
+    
+    func testPostHeaderSuccess() {
+        var wait: Bool = true
+        
+        let jsonData = NSJSONSerialization.dataWithJSONObject(["1","2","3"], options: nil, error: nil)
+        let jsonString = NSString(data: jsonData, encoding: NSUTF8StringEncoding)
+        
+        SRWebClient.POST("http://validate.jsontest.com/", data: ["json":jsonString],
+            headers:["Content-Type":"application/x-www-form-urlencoded charset=utf-8"],
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNotNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNil(error)
+                wait = false
+            })
+        self.waitFor(&wait)
+    }
+    
+    func testPostFailure() {
+        var wait: Bool = true
+        SRWebClient.POST("http://failure.example.com/",
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNotNil(error)
+                wait = false
+        })
+        self.waitFor(&wait)
+    }
+    
+    func testPostDataFailure() {
+        var wait: Bool = true
+        SRWebClient.POST("http://failure.example.com/", data: ["data1":"example","data2":"example"],
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNil(response)
+                wait = false
+            },
+            failure:{(error:NSError!) -> Void in
+                XCTAssertNotNil(error)
+                wait = false
+            })
+        self.waitFor(&wait)
+    }
+    
+    func testNilSuccessGet() {
+        var wait: Bool = true
+        SRWebClient.GET("http://headers.jsontest.com/",
+            success:{(response:AnyObject!, status:Int) -> Void in
+                XCTAssertNotNil(response)
+                wait = false
+            }, failure:nil)
+        self.waitFor(&wait)
+    }
+    
+    func testNilFailureGet() {
+        var wait: Bool = true
+        SRWebClient.GET("http://failure.example.com/",
+            success:nil, failure:{(error:NSError!) -> Void in
+                XCTAssertNotNil(error)
+                wait = false
+        })
+        self.waitFor(&wait)
+    }
+
+    func testNilPost() {
+        SRWebClient.POST("http://failure.example.com/", data: nil, success:nil, failure:nil)
+    }
+}
